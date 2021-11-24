ae01f4f14d7e90cac0f9d029b1c1644ca608a9c8
u49728rt <robert.taylor-11@student.manchester.ac.uk>
2021-11-24 02:20:41 +0000

    Added tentacle and basic score system

--- gamespace.py
+++ gamespace.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.ttk import *
 import math
+import random
 
 import time
 
@@ -44,6 +45,9 @@ class Vector2:
         return str(self.x) + " " + str(self.y)
 
 
+def debug(event):
+    print("WHAAAT")
+
 
 class Main:
     def __init__(self,root): #give clean root
@@ -51,19 +55,36 @@ class Main:
         self.height = root.winfo_height()
         self.width = root.winfo_width()
         self.root = Canvas(self.parent,width=self.width,height=self.height)
+
+
+        self.spawnCircle = max(self.width/2,self.height/2)
+
+        self.score = 0
+        self.scoreTextID = self.root.create_text(0,0, text="Score: 0", anchor=NW)
+
         self.root.place(x=0,y=0)
 
-        self.root.bind("<Button-1>",self.clickevent)
+        self.parent.bind("a",self.debug_makeItem)
+        self.parent.bind("<Button-1>",self.clickevent)
+        
         
         self.player = playerController(self,Vector2(self.width/2,self.height/2))
+
+        self.mousePos = Vector2(0,0)
+
         self.items = []
         self.deltatime = 0.0
-        self.makeItem()
         
         self.mainloop()
 
+    def debug_makeItem(self,event):
+        print("spawwwn")
+        self.makeItem()
+
     def makeItem(self):
-        i = Item(self,"placeholderItem.png",Vector2(0,0))
+        angle = random.uniform(0,math.pi*2)
+        spc = self.spawnCircle + 10 ## currently for size, although size may not change
+        i = Item(self,"placeholderItem.png",Vector2(math.cos(angle),math.sin(angle))*spc)
         self.items.append(i)
 
     def mainloop(self):
@@ -76,26 +97,40 @@ class Main:
             self.deltatime = curTime - preTime
             preTime = curTime
 
-            self.update()
+            self.mousePos.set(self.parent.winfo_pointerx()-self.parent.winfo_rootx(),self.parent.winfo_pointery()-self.parent.winfo_rooty())
+            self.player.tentacle.follow(self.mousePos)
+            self.player.update()
 
             ## Item Hit Detection
             for item in self.items:
+                item.update()
                 dist = (self.player.pos-item.pos).magnitude()
                 if dist < item.size+self.player.size:
+                    self.increaseScore(-100)
+                    self.items.remove(item)
                     print("blamo!")
-            
 
-    def update(self):
-        for item in self.items:
-            item.update()
-
-        self.parent.update()
+            self.parent.update()
+            self.parent.update_idletasks()
+        
 
     def clickevent(self,event):
+        toremove = []
         for item in self.items:
             dist = (Vector2(event.x,event.y)-item.pos).magnitude()
             if dist < item.size:
+                self.increaseScore(10)
+                print(len(self.items))
+                self.root.delete(item)
+                self.parent.update()
+                toremove.append(item)
                 print("pop!")
+        for i in toremove:
+            self.items.remove(i)
+
+    def increaseScore(self,nscore):
+        self.score += nscore
+        self.root.itemconfigure(self.scoreTextID,text="Score: "+str(self.score))
         
 
 class playerController:
@@ -105,6 +140,12 @@ class playerController:
         self.root = mainArea.root
         self.size = 10
         self.root.create_oval([self.pos.x-self.size,self.pos.y-self.size],[self.pos.x+self.size,self.pos.y+self.size])
+        self.tentacleLength = 20
+        joints = [Joint(Vector2(self.pos.x,self.pos.y + i*tentacleLength),tentacleLength, math.pi/2) for i in range(0,4)]
+        self.tentacle = Tentacle(joints,self.main)
+
+    def update(self):
+        self.tentacle.update()
 
 class Item:
     def __init__(self, mainArea, imgPath, startpos, speed=50):
@@ -129,3 +170,86 @@ class Item:
         self.pos += diff
         self.root.move(self.id,diff.x,diff.y)
         
+
+
+
+
+##INVERSE KINEMATICS FOR TENTACLE FOLLOW###
+
+class Joint:
+    def __init__(self,position,length,angle=-(math.pi/2)):
+        self.position = position
+        self.angle = angle
+        self.length = length
+
+class Tentacle:
+    def __init__(self,joints,area):
+        self.joints = joints
+        self.area = area
+        self.numJoints = len(self.joints)
+        self.end = joints[-1].position
+        self.validDistance = 1
+        self.setJoints()
+        
+        self.makeTentacles()
+        
+
+    def makeTentacles(self):
+        self.tentacles = []
+        for i in range(self.numJoints-1):
+            self.tentacles.append(self.area.root.create_line(self.jointPositions[i],self.jointPositions[i+1],width = 5-i*2))
+
+    def setJoints(self):
+        self.jointPositions = []
+        for jointId in range(self.numJoints-1):
+            self.joints[jointId+1].position = self.joints[jointId].position + Vector2(math.cos(self.joints[jointId].angle)*self.joints[jointId].length,math.sin(self.joints[jointId].angle)*self.joints[jointId].length)
+            self.jointPositions.append([self.joints[jointId].position.x,self.joints[jointId].position.y])
+        self.jointPositions.append([self.joints[-1].position.x,self.joints[-1].position.y])
+        self.end = self.joints[-1].position
+    
+    def update(self):
+        self.setJoints()
+        
+        for tentacle in self.tentacles:
+            self.area.root.delete(tentacle)
+        self.makeTentacles()
+
+    def follow(self,target):
+        finalJoint = self.joints[-1]
+        #self.end.set(finalJoint.position.x + math.cos(finalJoint.angle)*finalJoint.length, finalJoint.position.y + math.sin(finalJoint.angle)*finalJoint.length)
+        self.end.set(finalJoint.position.x, finalJoint.position.y)
+
+        a = (target-self.end)
+        jid = self.numJoints-1
+
+        timesRep = 0
+        
+        while a.magnitude() > self.validDistance and timesRep < 5:
+            j = self.joints[jid]
+            jtoe = self.end-j.position
+            jtot = target-j.position
+            etMag = jtoe.magnitude()*jtot.magnitude()
+            if etMag <= 0.000001: # avoid small division/div by 0
+                cosRot = 1
+                sinRot = 0
+            else:
+                cosRot = (jtoe.x*jtot.x + jtot.y*jtoe.y) / etMag
+                sinRot = (jtoe.x*jtot.y - jtoe.y*jtot.x) / etMag
+            
+            rotAng = math.acos(max(-1,min(1,cosRot)))
+
+            rotAng = min(rotAng,math.pi*self.area.deltatime)
+
+            if sinRot < 0:
+                rotAng = -rotAng
+            
+            self.setJoints()
+            self.end = self.joints[-1].position
+            j.angle += rotAng
+
+            a = (target-self.end)
+            if jid == 0:
+                timesRep += 1
+                self.setJoints()
+                jid = self.numJoints
+            jid -= 1
